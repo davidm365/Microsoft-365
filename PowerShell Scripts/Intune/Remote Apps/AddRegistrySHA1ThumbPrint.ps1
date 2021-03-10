adc12637c1ef8d267a65128433d794e5a8b96b31
David Theilman <64646384+davidm365@users.noreply.github.com>
2021-03-10 08:06:55 -0500

    Add files via upload

---
+++ PowerShell Scripts/Intune/Remote Apps/AddRegistrySHA1ThumbPrint.ps1
@@ -0,0 +1,8 @@
+﻿# Set the location to the registry
+Set-Location -Path 'HKLM:\Software\Policies\Microsoft\Windows NT\Terminal Services'
+
+# Create new items with values
+New-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows NT\Terminal Services' -Name 'TrustedCertThumbprints' -Value "7F98F4500F5DFBD7505E00B7C730855E2365716E,4C83ED92EDD626C3AF51FFE1BA0B97E63CC6245F" -PropertyType String -Force
+
+# Get out of the Registry
+Pop-Location
---
+++ PowerShell Scripts/Intune/Remote Apps/AddWorkspacesRegistry.ps1
@@ -0,0 +1,13 @@
+﻿# Set the location to the registry
+Set-Location -Path 'HKCU:\Software\Policies\Microsoft'
+
+# Create a new Key
+Get-Item -Path 'HKCU:\Software\Policies\Microsoft' | New-Item -Name 'Workspaces' -Force
+
+# Create new items with values
+New-ItemProperty -Path 'HKCU:\Software\Policies\Microsoft\Workspaces\' -Name 'DefaultConnectionURL' -Value "https://cchs-rds01.cchs.local/rdweb/feed/webfeed.aspx" -PropertyType String
+
+Set-ItemProperty -Path 'HKCU:\Software\Policies\Microsoft\Workspaces\' -Name '(Default)' -Value 0
+
+# Get out of the Registry
+Pop-Location
