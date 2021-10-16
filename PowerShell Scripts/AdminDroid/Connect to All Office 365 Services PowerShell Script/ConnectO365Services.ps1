4828181592ba302a6871f4449963be7f1faca8e2
David Theilman <64646384+davidm365@users.noreply.github.com>
2021-10-16 09:17:04 -0400

    Add files via upload

---
+++ PowerShell Scripts/AdminDroid/Connect to All Office 365 Services PowerShell Script/ConnectO365Services.ps1
@@ -0,0 +1,383 @@
+﻿
+Param
+(
+    [Parameter(Mandatory = $false)]
+    [switch]$Disconnect,
+    [ValidateSet('AzureAD','MSOnline','ExchangeOnline','SharePoint','SharePointPnP','SecAndCompCenter','Skype','Teams')]
+    [string[]]$Services=("AzureAD","MSOnline","ExchangeOnline",'SharePoint','SharePointPnP','SecAndCompCenter','Skype','Teams'),
+    [string]$SharePointHostName,
+    [Switch]$MFA,
+    [string]$UserName, 
+    [string]$Password
+)
+ 
+#Disconnecting Sessions
+if($Disconnect.IsPresent)
+{
+ #Disconnect Exchange Online,Skype and Security & Compliance center session
+ Get-PSSession | Remove-PSSession
+ #Disconnect Teams connection
+ Disconnect-MicrosoftTeams
+ #Disconnect SharePoint connection
+ Disconnect-SPOService
+ Write-Host All sessions in the current window has been removed. -ForegroundColor Yellow
+}
+ 
+else
+{
+ if(($UserName -ne "") -and ($Password -ne "")) 
+ { 
+  $SecuredPassword = ConvertTo-SecureString -AsPlainText $Password -Force 
+  $Credential  = New-Object System.Management.Automation.PSCredential $UserName,$SecuredPassword 
+ } 
+
+ #Getting credential for non-MFA account
+ elseif(!($MFA.IsPresent)) 
+ { 
+  $Credential=Get-Credential -Credential $null
+ } 
+ $ConnectedServices=""
+ if($Services.Length -eq 8)
+ {
+  $RequiredServices=$Services  
+ }
+ else
+ {
+  $RequiredServices=$PSBoundParameters.Services
+ }
+
+ #Loop through each required services
+ Foreach($Service in $RequiredServices)
+ {
+  Write-Host Checking connection to $Service...
+  Switch ($Service)
+  {  
+   #Module and Connection settings for Exchange Online module
+   ExchangeOnline
+   {
+    $Module=Get-InstalledModule -Name ExchangeOnlineManagement -MinimumVersion 2.0.3
+    if($Module.count -eq 0)
+    {
+     Write-Host Required Exchange Online'(EXO V2)' module is not available  -ForegroundColor yellow 
+     $Confirm= Read-Host Are you sure you want to install module? [Y] Yes [N] No
+     if($Confirm -match "[yY]")
+     {
+      Install-Module ExchangeOnlineManagement
+      Import-Module ExchangeOnlineManagement
+     }
+     else
+     {
+      Write-Host EXO V2 module is required to connect Exchange Online.Please install module using Install-Module ExchangeOnlineManagement cmdlet.
+     }
+     Continue
+    }
+    if($mfa.IsPresent)
+    {
+     Connect-ExchangeOnline
+    }
+    else
+    {
+     Connect-ExchangeOnline -Credential $Credential
+    }
+    If((Get-EXOMailbox -ResultSize 1) -ne $null)
+    {
+     if($ConnectedServices -ne "")
+     {
+      $ConnectedServices=$ConnectedServices+","
+     }
+     $ConnectedServices=$ConnectedServices+" Exchange Online"
+    }
+   }
+
+  #Module and Connection settings for AzureAD V1 (MSOnline module)
+   MSOnline
+   {
+    $Module=Get-Module -Name MSOnline -ListAvailable 
+    if($Module.count -eq 0)
+    {
+     Write-Host MSOnline module is not available  -ForegroundColor yellow 
+     $Confirm= Read-Host Are you sure you want to install module? [Y] Yes [N] No
+     if($Confirm -match "[yY]")
+     {
+      Install-Module MSOnline
+      Import-Module MSOnline
+     }
+     else
+     {
+      Write-Host MSOnline module is required to connect AzureAD.Please install module using Install-Module MSOnline cmdlet.
+     }
+     Continue
+    }
+    if($mfa.IsPresent)
+    {
+     Connect-MsolService
+    }
+    else
+    {
+     Connect-MsolService -Credential $Credential
+    }
+    If((Get-MsolUser -MaxResults 1) -ne $null)
+    {
+     if($ConnectedServices -ne "")
+     {
+      $ConnectedServices=$ConnectedServices+","
+     }
+     $ConnectedServices=$ConnectedServices+" MSOnline"
+    }
+    if(($RequiredServices -contains "SharePoint") -eq "true")
+     {
+      $SharePointHostName=((Get-MsolDomain) | where {$_.IsInitial -eq "True"} ).name -split ".onmicrosoft.com"
+      $SharePointHostName=($SharePointHostName).trim()
+     }
+   }
+
+
+
+   #Module and Connection settings for AzureAD V2 (AzureAD module)
+   AzureAD
+   {
+    $Module=Get-Module -Name AzureAD -ListAvailable 
+    if($Module.count -eq 0)
+    {
+     Write-Host AzureAD module is not available  -ForegroundColor yellow 
+     $Confirm= Read-Host Are you sure you want to install module? [Y] Yes [N] No
+     if($Confirm -match "[yY]")
+     {
+      Install-Module AzureAD
+      Import-Module AzureAD
+     }
+     else
+     {
+      Write-Host AzureAD module is required to connect AzureAD.Please install module using Install-Module AzureAD cmdlet.
+     }
+     Continue
+    }
+    if($mfa.IsPresent)
+    {
+     Connect-AzureAD
+    }
+    else
+    {
+     Connect-AzureAD -Credential $Credential
+    }
+    If((Get-AzureADUser -Top 1) -ne $null)
+    {
+     if($ConnectedServices -ne "")
+     {
+      $ConnectedServices=$ConnectedServices+","
+     }
+     $ConnectedServices=$ConnectedServices+" AzureAD"
+     
+    }
+   }
+
+   #Module and Connection settings for SharePoint Online module
+   SharePoint
+   {
+    $Module=Get-Module -Name Microsoft.Online.SharePoint.PowerShell -ListAvailable 
+    if($Module.count -eq 0)
+    {
+     Write-Host SharePoint Online PowerShell module is not available  -ForegroundColor yellow 
+     $Confirm= Read-Host Are you sure you want to install module? [Y] Yes [N] No
+     if($Confirm -match "[yY]")
+     {
+      Install-Module Microsoft.Online.SharePoint.PowerShell
+     }
+     else
+     {
+      Write-Host SharePoint Online PowerShell module is required.Please install module using Install-Module Microsoft.Online.SharePoint.PowerShell cmdlet.
+      Continue
+     }
+    }
+    if(!($PSBoundParameters['SharePointHostName']) -and ([string]$SharePointHostName -eq "") ) 
+    {
+     Write-Host SharePoint organization name is required.`nEg: Contoso for admin@Contoso.Onmicrosoft.com -ForegroundColor Yellow
+     $SharePointHostName= Read-Host "Please enter SharePoint organization name"  
+    }
+     
+    if($MFA.IsPresent)
+    {
+     Import-Module Microsoft.Online.SharePoint.PowerShell -DisableNameChecking
+     Connect-SPOService -Url https://$SharePointHostName-admin.sharepoint.com
+    }
+    else
+    {
+     Import-Module Microsoft.Online.SharePoint.PowerShell -DisableNameChecking
+     Connect-SPOService -Url https://$SharePointHostName-admin.sharepoint.com -credential $credential
+    } 
+    if((Get-SPOTenant) -ne $null)
+    {
+     if($ConnectedServices -ne "")
+     {
+      $ConnectedServices=$ConnectedServices+","
+     }
+     $ConnectedServices=$ConnectedServices+"SharePoint Online"
+    }
+   }
+
+   #Module and Connection settings for Sharepoint PnP module
+   SharePointPnP
+   {
+    $Module=Get-InstalledModule -Name SharePointPnPPowerShellOnline
+    if($Module.count -eq 0)
+    {
+     Write-Host SharePoint PnP module module is not available  -ForegroundColor yellow 
+     $Confirm= Read-Host Are you sure you want to install module? [Y] Yes [N] No
+     if($Confirm -match "[yY]")
+     {
+      Install-Module -Name SharePointPnPPowerShellOnline -AllowClobber
+     }
+     else
+     {
+      Write-Host SharePoint Pnp module is required.Please install module using Install-Module SharePointPnPPowerShellOnline cmdlet.
+     }
+     Continue
+    }
+    if(!($PSBoundParameters['SharePointHostName']) -and ([string]$SharePointHostName -eq "") ) 
+    {
+     Write-Host SharePoint organization name is required.`nEg: Contoso for admin@Contoso.com -ForegroundColor Yellow
+     $SharePointHostName= Read-Host "Please enter SharePoint organization name"  
+    }
+     
+    if($MFA.IsPresent)
+    {
+     Import-Module SharepointpnpPowerShellOnline -DisableNameChecking
+     Connect-PnPOnline -Url https://$SharePointHostName.sharepoint.com -UseWebLogin -WarningAction Ignore
+    }
+    else
+    {
+     Import-Module SharepointpnpPowerShellOnline -DisableNameChecking
+     Connect-PnPOnline -Url https://$SharePointHostName.sharepoint.com -credential $credential -WarningAction Ignore
+    } 
+    If ($? -eq $true)
+    {
+     if($ConnectedServices -ne "")
+     {
+      $ConnectedServices=$ConnectedServices+","
+     }
+     $ConnectedServices=$ConnectedServices+"SharePoint PnP"  
+    }
+   }
+
+
+   #Module and Connection settings for Skype for Business Online module
+   Skype
+   { 
+    $Module=Get-InstalledModule -Name MicrosoftTeams -MinimumVersion 1.1.6 
+    if($Module.count -eq 0)
+    {
+     Write-Host Required MicrosoftTeams module is not available  -ForegroundColor yellow 
+     $Confirm= Read-Host Are you sure you want to install module? [Y] Yes [N] No
+     if($Confirm -match "[yY]")
+     {
+      Install-Module MicrosoftTeams -AllowClobber
+     }
+     else
+     {
+      Write-Host MicrosoftTeams module is required.Please install module using Install-Module MicrosoftTeams cmdlet.
+     }
+     Continue
+    }
+    if($MFA.IsPresent)
+    {
+     $sfbSession = New-CsOnlineSession
+     Import-PSSession $sfbSession -AllowClobber | Out-Null
+    }
+    else
+    {
+     $sfbSession = New-CsOnlineSession -Credential $Credential
+     Import-PSSession $sfbSession -AllowClobber -WarningAction SilentlyContinue | Out-Null
+    }
+    #Check for Skype connectivity
+    If ((Get-PSSession | Where-Object { $_.ConfigurationName -like "Microsoft.PowerShell" }) -ne $null)
+    {
+     if($ConnectedServices -ne "")
+     {
+      $ConnectedServices=$ConnectedServices+","
+     }
+     $ConnectedServices=$ConnectedServices+"Skype"  
+    }
+   }
+
+   #Module and Connection settings for Security & Compliance center
+   SecAndCompCenter
+   {
+    $Module=Get-InstalledModule -Name ExchangeOnlineManagement -MinimumVersion 2.0.3
+    if($Module.count -eq 0)
+    {
+     Write-Host Exchange Online'(EXO V2)' module is not available  -ForegroundColor yellow 
+     $Confirm= Read-Host Are you sure you want to install module? [Y] Yes [N] No
+     if($Confirm -match "[yY]")
+     {
+      Install-Module ExchangeOnlineManagement
+      Import-Module ExchangeOnlineManagement
+     }
+     else
+     {
+      Write-Host EXO V2 module is required to connect Security and Compliance PowerShell.Please install module using Install-Module ExchangeOnlineManagement cmdlet.
+     }
+     Continue
+    }
+    if($mfa.IsPresent)
+    {
+     Connect-IPPSSession -WarningAction SilentlyContinue
+    }
+    else
+    {
+     Connect-IPPSSession -Credential $Credential -WarningAction SilentlyContinue
+    }
+    $Result=Get-RetentionCompliancePolicy
+    If(($?) -eq $true)
+    {
+     if($ConnectedServices -ne "")
+     {
+      $ConnectedServices=$ConnectedServices+","
+     }
+     $ConnectedServices=$ConnectedServices+" Security & Compliance Center"
+    }
+   }
+  
+   #Module and Connection settings for Teams Online module
+   Teams
+   {
+    $Module=Get-InstalledModule -Name MicrosoftTeams -MinimumVersion 1.1.6 
+    if($Module.count -eq 0)
+    {
+     Write-Host Required MicrosoftTeams module is not available  -ForegroundColor yellow 
+     $Confirm= Read-Host Are you sure you want to install module? [Y] Yes [N] No
+     if($Confirm -match "[yY]")
+     {
+      Install-Module MicrosoftTeams -AllowClobber
+     }
+     else
+     {
+      Write-Host MicrosoftTeams module is required.Please install module using Install-Module MicrosoftTeams cmdlet.
+     }
+     Continue
+    }
+    if($mfa.IsPresent)
+    {
+     $Team=Connect-MicrosoftTeams
+    }
+    else
+    {
+     $Team=Connect-MicrosoftTeams -Credential $Credential
+    }
+    #Check for Teams connectivity
+    If($Team -ne $null)
+    {
+     if($ConnectedServices -ne "")
+     {
+      $ConnectedServices=$ConnectedServices+","
+     }
+     $ConnectedServices=$ConnectedServices+"Teams"
+    }
+   }
+  }
+ }
+ if($ConnectedServices -eq "")
+ {
+  $ConnectedServices="-"
+ }
+ Write-Host `n`nConnected Services $ConnectedServices -ForegroundColor DarkYellow 
+}
---
+++ PowerShell Scripts/AdminDroid/Office 365 User Last Logon Time Report/LastLogonTimeReport.ps1
@@ -0,0 +1,221 @@
+﻿<#
+=============================================================================================
+Name:           Export Office 365 user last logon time report
+Description:    This script exports Office 365 users' last logon time CSV
+Version:        3.0
+website:        o365reports.com
+Script by:      O365Reports Team
+For detailed Script execution: https://o365reports.com/2019/03/07/export-office-365-users-last-logon-time-csv/
+============================================================================================
+#>
+#Accept input parameter
+Param
+(
+    [Parameter(Mandatory = $false)]
+    [int]$InactiveDays,
+    [switch]$UserMailboxOnly,
+    [switch]$ReturnNeverLoggedInMB,
+    [string]$UserName,
+    [string]$Password,
+    [switch]$MFA
+
+)
+
+#Check for MSOnline module 
+$Module=Get-Module -Name MSOnline -ListAvailable  
+if($Module.count -eq 0) 
+{ 
+ Write-Host MSOnline module is not available  -ForegroundColor yellow  
+ $Confirm= Read-Host Are you sure you want to install module? [Y] Yes [N] No 
+ if($Confirm -match "[yY]") 
+ { 
+  Install-Module MSOnline 
+  Import-Module MSOnline
+ } 
+ else 
+ { 
+  Write-Host MSOnline module is required to connect AzureAD.Please install module using Install-Module MSOnline cmdlet. 
+  Exit
+ }
+} 
+
+#Clear session
+Get-PSSession | Remove-PSSession
+
+#Get friendly name of license plan from external file
+$FriendlyNameHash=Get-Content -Raw -Path .\LicenseFriendlyName.txt -ErrorAction Stop | ConvertFrom-StringData
+
+
+#Set output file
+$ExportCSV=".\LastLogonTimeReport_$((Get-Date -format yyyy-MMM-dd-ddd` hh-mm` tt).ToString()).csv"
+
+ #Check for EXO v2 module inatallation
+ $Module = Get-Module ExchangeOnlineManagement -ListAvailable
+ if($Module.count -eq 0) 
+ { 
+  Write-Host Exchange Online PowerShell V2 module is not available  -ForegroundColor yellow  
+  $Confirm= Read-Host Are you sure you want to install module? [Y] Yes [N] No 
+  if($Confirm -match "[yY]") 
+  { 
+   Write-host "Installing Exchange Online PowerShell module"
+   Install-Module ExchangeOnlineManagement -Repository PSGallery -AllowClobber -Force
+  } 
+  else 
+  { 
+   Write-Host EXO V2 module is required to connect Exchange Online.Please install module using Install-Module ExchangeOnlineManagement cmdlet. 
+   Exit
+  }
+ } 
+ 
+ #Connect Exchange Online with MFA
+ if($MFA.IsPresent)
+ {
+  Write-Host Connecting Exchange Online...
+  Connect-ExchangeOnline | Out-Null
+  Write-Host Connecting to Office 365...
+  Connect-MsolService | Out-Null
+ }
+
+ #Authentication using non-MFA
+ else
+ {
+  #Storing credential in script for scheduling purpose/ Passing credential as parameter
+  if(($UserName -ne "") -and ($Password -ne ""))
+  {
+   $SecuredPassword = ConvertTo-SecureString -AsPlainText $Password -Force
+   $Credential  = New-Object System.Management.Automation.PSCredential $UserName,$SecuredPassword
+  }
+  else
+  {
+   $Credential=Get-Credential -Credential $null
+  }
+  Write-Host Connecting Exchange Online...
+  Connect-ExchangeOnline -Credential $Credential
+  Write-Host Connecting to Office 365...
+  Connect-MsolService -Credential $Credential
+ }
+
+$Result=""
+$Output=@()
+$MBUserCount=0
+$OutputCount=0
+
+
+Get-Mailbox -ResultSize Unlimited | Where{$_.DisplayName -notlike "Discovery Search Mailbox"} | ForEach-Object{
+ $upn=$_.UserPrincipalName
+ $CreationTime=$_.WhenCreated
+ $LastLogonTime=(Get-MailboxStatistics -Identity $upn).lastlogontime
+ $DisplayName=$_.DisplayName
+ $MBType=$_.RecipientTypeDetails
+ $Print=1
+ $MBUserCount++
+ $RolesAssigned=""
+ Write-Progress -Activity "`n     Processed mailbox count: $MBUserCount "`n"  Currently Processing: $DisplayName"
+
+ #Retrieve lastlogon time and then calculate Inactive days
+ if($LastLogonTime -eq $null)
+ {
+   $LastLogonTime ="Never Logged In"
+   $InactiveDaysOfUser="-"
+ }
+ else
+ {
+   $InactiveDaysOfUser= (New-TimeSpan -Start $LastLogonTime).Days
+ }
+
+ #Get licenses assigned to mailboxes
+ $User=(Get-MsolUser -UserPrincipalName $upn)
+ $Licenses=$User.Licenses.AccountSkuId
+ $AssignedLicense=""
+ $Count=0
+
+ #Convert license plan to friendly name
+ foreach($License in $Licenses)
+ {
+    $Count++
+    $LicenseItem= $License -Split ":" | Select-Object -Last 1
+    $EasyName=$FriendlyNameHash[$LicenseItem]
+    if(!($EasyName))
+    {$NamePrint=$LicenseItem}
+    else
+    {$NamePrint=$EasyName}
+    $AssignedLicense=$AssignedLicense+$NamePrint
+    if($count -lt $licenses.count)
+    {
+      $AssignedLicense=$AssignedLicense+","
+    }
+ }
+ if($Licenses.count -eq 0)
+ {
+  $AssignedLicense="No License Assigned"
+ }
+
+ #Inactive days based filter
+ if($InactiveDaysOfUser -ne "-"){
+ if(($InactiveDays -ne "") -and ([int]$InactiveDays -gt $InactiveDaysOfUser))
+ {
+  $Print=0
+ }}
+
+ #License assigned based filter
+ if(($UserMailboxOnly.IsPresent) -and ($MBType -ne "UserMailbox"))
+ {
+  $Print=0
+ }
+
+ #Never Logged In user
+ if(($ReturnNeverLoggedInMB.IsPresent) -and ($LastLogonTime -ne "Never Logged In"))
+ {
+  $Print=0
+ }
+
+ #Get roles assigned to user
+ $Roles=(Get-MsolUserRole -UserPrincipalName $upn).Name
+ if($Roles.count -eq 0)
+ {
+  $RolesAssigned="No roles"
+ }
+ else
+ {
+  foreach($Role in $Roles)
+  {
+   $RolesAssigned=$RolesAssigned+$Role
+   if($Roles.indexof($role) -lt (($Roles.count)-1))
+   {
+    $RolesAssigned=$RolesAssigned+","
+   }
+  }
+ }
+
+ #Export result to CSV file
+ if($Print -eq 1)
+ {
+  $OutputCount++
+  $Result=@{'UserPrincipalName'=$upn;'DisplayName'=$DisplayName;'LastLogonTime'=$LastLogonTime;'CreationTime'=$CreationTime;'InactiveDays'=$InactiveDaysOfUser;'MailboxType'=$MBType; 'AssignedLicenses'=$AssignedLicense;'Roles'=$RolesAssigned}
+  $Output= New-Object PSObject -Property $Result
+  $Output | Select-Object UserPrincipalName,DisplayName,LastLogonTime,CreationTime,InactiveDays,MailboxType,AssignedLicenses,Roles | Export-Csv -Path $ExportCSV -Notype -Append
+ }
+}
+
+#Open output file after execution
+Write-Host `nScript executed successfully
+if((Test-Path -Path $ExportCSV) -eq "True")
+{
+ 
+ Write-Host "Detailed report available in: $ExportCSV"
+ 
+ Write-Host Exported report has $OutputCount mailboxes
+ $Prompt = New-Object -ComObject wscript.shell
+ $UserInput = $Prompt.popup("Do you want to open output file?",`
+ 0,"Open Output File",4)
+ If ($UserInput -eq 6)
+ {
+  Invoke-Item "$ExportCSV"
+ }
+}
+Else
+{
+  Write-Host No mailbox found
+}
+#Clean up session
+Get-PSSession | Remove-PSSession
