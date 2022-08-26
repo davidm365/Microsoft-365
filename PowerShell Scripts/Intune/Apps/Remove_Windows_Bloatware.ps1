51239b65968f0b6510c136fea5f6e0ff40c836e8
David Theilman <64646384+davidm365@users.noreply.github.com>
2022-08-26 09:56:36 -0400

    Add files via upload

---
+++ PowerShell Scripts/Intune/Apps/Remove_Windows_Bloatware.ps1
@@ -0,0 +1,9 @@
+Get-AppxPackage Microsoft.WindowsCamera | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxIdentityProvider | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxGameCallableUI | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxSpeechToTextOverlay | Remove-AppxPackage
+Get-AppxPackage Microsoft.XboxGameOverlay | Remove-AppxPackage
+Get-AppxPackage Microsoft.Xbox.TCUI | Remove-AppxPackage
+Get-AppxPackage Microsoft.WindowsSoundRecorder | Remove-AppxPackage
+Get-AppxPackage Microsoft.Wallet | Remove-AppxPackage
+
